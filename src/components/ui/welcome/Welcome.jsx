e8c8b0aea661877c6efa9f06cc03a4e20f5b1d15
Dmitriy Pidko <pidkodima@gmail.com>
2024-04-22 15:39:51 +0300

    add img slider

--- src/components/ui/welcome/Welcome.jsx
+++ src/components/ui/welcome/Welcome.jsx
@@ -1,5 +1,23 @@
+import images from './data/images';
+
+import CustomCarousel from '../../custom/slider/Slider';
+
 const Welcome = () => {
-    return <section className="welcome"></section>;
+    return (
+        <section className="welcome">
+            <CustomCarousel>
+                {images.map((image, index) => {
+                    return (
+                        <img
+                            key={index}
+                            src={image.imgURL}
+                            alt={image.imgAlt}
+                        />
+                    );
+                })}
+            </CustomCarousel>
+        </section>
+    );
 };
 
 export default Welcome;
