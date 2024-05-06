b56113e6ad91d5e421fec0a8e5861bd99ac6f33c
Dmitriy Pidko <pidkodima@gmail.com>
2024-05-06 10:15:29 +0300

    mobile version: add animated for callback btn

--- src/components/ui/contacts/ContactsBlock.jsx
+++ src/components/ui/contacts/ContactsBlock.jsx
@@ -9,14 +9,16 @@ const Contacts = () => {
             <div className="contacts__phone">
                 <a
                     className="contacts__phone-num"
-                    href="tel:+380503000719">
+                    href="tel:+380503000719"
+                    target="_blank">
                     +38-(050)-300-07-19
                 </a>
             </div>
             <div className="contacts__phone">
                 <a
                     className="contacts__phone-num"
-                    href="https://invite.viber.com/?g2=AQBXiKigTFNdUEyez1usc0d%2FJzIte8ziDAibfn0QsHzkE%2B9ZsP9KVVd1zz3dRqoA">
+                    href="https://invite.viber.com/?g2=AQBXiKigTFNdUEyez1usc0d%2FJzIte8ziDAibfn0QsHzkE%2B9ZsP9KVVd1zz3dRqoA"
+                    target="_blank">
                     Viber
                 </a>
             </div>
